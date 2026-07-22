e1ee7b1629118db8fed882d4f1d9e39ff2338c2a
hassanakhtar24 <hassanakhtar24@gmail.com>
2026-07-23 04:00:23 +0500

    revert logo changes

--- src/data/logo.js
+++ src/data/logo.js
@@ -1,6 +1,4 @@
-import logoImg from "../assets/images/logo/logo-01.png";
-
 export const logo = {
-  light: "/logo-01.png",
-  dark: "/logo-01.png",
+  light: "/assets/images/logo/logo-01.png",
+  dark: "/assets/images/logo/logo-01.png",
 };
